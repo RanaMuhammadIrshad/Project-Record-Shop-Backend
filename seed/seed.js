19ec133528c88231b343cc9089609c81368f0f3f
RanaMuhammadIrshad <rmirshad@web.de>
2022-11-03 12:16:30 +0100

    The mongoose conection is established, schemas for users, records and orders are prepared and used faker package to take dummy data for records and users

---
+++ seed/seed.js
@@ -0,0 +1,48 @@
+import mongoose from 'mongoose';
+import recordsCollection from '../models/recordsSchema.js';
+import { faker } from '@faker-js/faker';
+import usersCollection from '../models/usersSchema.js';
+
+mongoose.connect('mongodb://127.0.0.1:27017/record-shop-rana', () => {
+  console.log('SEED connected to DB !!!');
+});
+
+async function addRecords() {
+  const recordPromises = Array(20)
+    .fill(null)
+    .map(() => {
+      const record = new recordsCollection({
+        title: faker.commerce.productName(),
+        author: faker.name.fullName(),
+        year: faker.date.past().getFullYear(),
+        img: faker.image.image(),
+        price: faker.commerce.price(),
+      });
+      return record.save();
+    });
+
+  await Promise.all(recordPromises);
+  mongoose.connection.close();
+}
+
+// addRecords();
+
+async function addUsers() {
+  const userPromises = Array(20)
+    .fill(null)
+    .map(() => {
+      const user = new usersCollection({
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        email: faker.internet.email(),
+        password: faker.internet.password(),
+      });
+      return user.save();
+    });
+
+  await Promise.all(userPromises);
+  console.log('20 users are stored in Data base');
+  mongoose.connection.close();
+}
+
+// addUsers();
